444db6d60c3fc33288954c2ac63758d3ffbed86a
Kartavya Gohil <83202563+badamnshake@users.noreply.github.com>
2024-09-08 21:13:53 +1000

    some testing and mitre code

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 #  pip install mitreattack-python
 # docs: https://mitreattack-python.readthedocs.io/en/latest/
 # docs: https://github.com/mitre-attack/mitreattack-python
+
+# import mitreattack.attackToExcel.attackToExcel as attackToExcel
+# import mitreattack.attackToExcel.stixToDf as stixToDf
