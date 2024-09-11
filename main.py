0cd50563d07e2c5131216d8147c8de480ea2aab4
Kartavya Gohil <83202563+badamnshake@users.noreply.github.com>
2024-09-11 22:53:34 +1000

    refactoring_arranging_code

--- main.py
+++
@@ -1,6 +0,0 @@
-#  pip install mitreattack-python
-# docs: https://mitreattack-python.readthedocs.io/en/latest/
-# docs: https://github.com/mitre-attack/mitreattack-python
-
-# import mitreattack.attackToExcel.attackToExcel as attackToExcel
-# import mitreattack.attackToExcel.stixToDf as stixToDf
